ab79c3b2fa50397801d9031e27906cda78b68671
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2025-06-11 22:24:54 +0200

    Document and valudeate

--- module.sv-geolocation.php
+++ module.sv-geolocation.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'sv-geolocation/2.0.0',
+	'sv-geolocation/2.0.1',
 	array(
 		// Identification
 		//
